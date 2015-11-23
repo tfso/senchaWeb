1480c289e46a535ab6d83d58e12f663ddd5cb508
Steinar Jordbakk <sjordb@frisurf.no>
2015-11-23 08:13:56 +0100

    First upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var server = express();
 server.use(express.static(__dirname + '/web'));
 
-var port = 3000;
+var port = 6262;
 server.listen(port, function() {
     console.log('server listening on port ' + port);
 });
