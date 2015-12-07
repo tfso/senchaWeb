09e0c74895849569dadfe3877308bd3868108336
Steinar Jordbakk <sjordb@frisurf.no>
2015-12-07 14:16:48 +0100

    test

--- server.js
+++ server.js
@@ -6,5 +6,6 @@ server.use(express.static(__dirname + '/web'));
 
 var port = 6262;
 server.listen(port, function() {
+	console.log('good times')
     console.log('server listening on port ' + port);
 });
